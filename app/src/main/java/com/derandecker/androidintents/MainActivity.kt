ad820d69e7f466c03fc68098b3e0b34d25f1fd5d
Deran Decker <me@derandecker.com>
2019-08-07 12:31:50 -1000

    Add image select. Uri is now shown when selecting an image. Need to update to show image name.

--- app/src/main/java/com/derandecker/androidintents/MainActivity.kt
+++ app/src/main/java/com/derandecker/androidintents/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.derandecker.androidintents
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -9,6 +11,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        internal const val REQUEST_IMAGE_INT = 1
+    }
 
     val imageList: ArrayList<ImageData> = ArrayList()
 
@@ -17,18 +22,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val deran: Uri = Uri.parse("Deran://ddd")
-        imageList.add(ImageData(deran))
-        imageList.add(ImageData(deran))
-        Log.d("ImageData Test", imageList[0].imageUriString)
+//        val deran: Uri = Uri.parse("Deran://ddd")
+//        imageList.add(ImageData(deran))
+//        imageList.add(ImageData(deran))
+//        Log.d("ImageData Test", imageList[0].imageUriString)
+
+        image_select_button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, REQUEST_IMAGE_INT)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_INT && resultCode == RESULT_OK) {
+            val imageUri = data!!.data
+            if (imageUri != null) {
+                imageList.add(ImageData(imageUri))
+            }
+        }
+
         populateData()
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 
     fun createTextView(image: ImageData): TextView {
         val view = TextView(this)
 
-        view.text = "${image.name}"
+        view.text = "${image.imageUriString}"
 
         return view
     }
